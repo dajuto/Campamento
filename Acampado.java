16de4b1a660edfce6aab92188e571f644209c1c1
david <davidvie@ucm.es>
2022-03-10 22:44:48 +0100

    cambios

---
+++ Acampado.java
@@ -0,0 +1,17 @@
+[33mcommit c5eb7de7c21abe90047911dd70466c8b7642b481[m[33m ([m[1;36mHEAD -> [m[1;32mdavid[m[33m)[m
+Author: david <davidvie@ucm.es>
+Date:   Thu Mar 10 16:29:07 2022 +0100
+
+    campos acampado
+
+[1mdiff --git a/src/clases/Acampado.java b/src/clases/Acampado.java[m
+[1mindex 38a3bb4..6deaaf2 100644[m
+[1m--- a/src/clases/Acampado.java[m
+[1m+++ b/src/clases/Acampado.java[m
+[36m@@ -1,5 +1,5 @@[m
+ package clases;[m
+ [m
+ public class Acampado {[m
+[31m-[m
+[32m+[m	[32mint salud, posCovid;//Atributos que tiene que llevar el acampado para controlar pandemia[m
+ }[m
