0991e86a25da69b783bbc651ef815a52ebe9fee6
david <davidvie@ucm.es>
2022-04-18 20:58:51 +0200

    avance david

--- src/sanidad/Negocio/TReceta.java
+++ src/sanidad/Negocio/TReceta.java
@@ -1,5 +1,15 @@
 package sanidad.Negocio;
 
-public class TReceta {
+import empleados.Negocio.Empleado;
+
+public class TReceta extends Receta{
+	
+	public TReceta(int codigo, String medicamento, String dosis, Empleado Nombremedico, String comprado){
+		this.codigo = codigo;
+		this.medicamento = medicamento;
+		this.dosis = dosis;
+		this.Nombremedico = Nombremedico;
+		this.comprado=comprado;
+	}
 
 }
