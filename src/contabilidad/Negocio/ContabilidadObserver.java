e6e6261ab002055c8b5e26241ba52bbb39aba73a
Adri <adriavic@ucm.es>
2022-04-19 16:21:22 +0200

    Addr

--- src/contabilidad/Negocio/ContabilidadObserver.java
+++ src/contabilidad/Negocio/ContabilidadObserver.java
@@ -4,8 +4,8 @@ import java.util.List;
 public interface ContabilidadObserver {
 
 	void onRegister(List<TGastos> listaGastos, List<TIngresos> listaIngresos, String nombreUsuario);
-	void onCreate(List<TGastos> listaLimpieza, List<TIngresos> listaIngresos, String nombreUsuario);
-	void onActualizar(List<Gastos> listaLimpieza, List<TIngresos> listaIngresos, String nombreUsuario);
+	void onCreate(List<TGastos> listaGastos, List<TIngresos> listaIngresos, String nombreUsuario);
+	void onActualizar(List<Gastos> listaGastos, List<TIngresos> listaIngresos, String nombreUsuario);
 }
 
 
