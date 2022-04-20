fb1cdf0d2f7345a804a2d0df714d140493da9573
david <davidvie@ucm.es>
2022-04-20 13:38:37 +0200

    r

---
+++ src/sanidad/Integracion/DaoSanidad.java
@@ -0,0 +1,20 @@
+package sanidad.Integracion;
+
+import java.util.List;
+
+import launcher.Factory;
+import sanidad.Negocio.TReceta;
+
+public class DaoSanidad {
+
+	public List<TReceta> leeTodo(Factory<Object> factoriaTranserObjects) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void escribeTodo(List<TReceta> listaRecetas) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
