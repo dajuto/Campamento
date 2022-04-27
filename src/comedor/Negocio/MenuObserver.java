16f139c226fc19796366364300ad7310f94e4b91
crbelt02 <101597954+crbelt02@users.noreply.github.com>
2022-04-28 00:39:20 +0200

    c

--- src/comedor/Negocio/MenuObserver.java
+++ src/comedor/Negocio/MenuObserver.java
@@ -1,12 +1,10 @@
-package comedor.negocio;
+package comedor.Negocio;
 
 import java.util.List;
 
 public interface MenuObserver {
-
+	void onRegister(List<TMenu> listaMenu);
 	void onCrearMenu(List<TMenu> listaMenu);
-	void onModificarMenu(List<TMenu> listaMenu);
 	void onEliminarMenu(List<TMenu> listaMenu);
 	void onConsultarMenu(List<TMenu> listaMenu);
-	
 }
