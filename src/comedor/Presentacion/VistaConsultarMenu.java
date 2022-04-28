6975c668290b8ef34ccb00ffc5f20a4113ee0f36
crbelt02 <101597954+crbelt02@users.noreply.github.com>
2022-04-28 02:35:01 +0200

    c

--- src/comedor/Presentacion/VistaConsultarMenu.java
+++ src/comedor/Presentacion/VistaConsultarMenu.java
@@ -7,11 +7,16 @@ import java.awt.SystemColor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
+import java.awt.BorderLayout;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JComponent;
+import javax.swing.JScrollPane;
+
 
 
 import comedor.Negocio.MenuObserver;
