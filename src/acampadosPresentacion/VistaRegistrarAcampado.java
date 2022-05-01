2dfd6d824db1ea02e97e8e1017609ddd532458fc
david <davidvie@ucm.es>
2022-05-01 14:23:48 +0200

    dav

--- src/acampadosPresentacion/VistaRegistrarAcampado.java
+++ src/acampadosPresentacion/VistaRegistrarAcampado.java
@@ -158,6 +158,8 @@ private String salud = "Sano";
 			public void actionPerformed(ActionEvent e) {
 				if (checkBox.isSelected()) {
 					salud = comboBox.getSelectedItem().toString();
+					SingletonControllerAcampado.getInstance().citaNuevoAcampado(getFrame(),salud,usuario.getText());
+					
 				}
 				if (password.getText().length() > 7 ) {
 					if (password.getText().equals(password2.getText())) {
@@ -167,8 +169,7 @@ private String salud = "Sano";
 									if (telefono.getText().matches("[0-9]*") && telefono.getText().length() == 9) {
 										SingletonControllerAcampado.getInstance().crearAcampado(usuario.getText(), password.getText(), nombre.getText(), apellidos.getText(), dni.getText(), email.getText(), edad.getText(), telefono.getText(), salud, getFrame());
 										SingletonControllerAcampado.getInstance().registraUsuario(usuario.getText());
-										SingletonControllerAcampado.getInstance().citaNuevoAcampado(getFrame(),salud,usuario.getText());
-									}
+										}
 									else JOptionPane.showMessageDialog(atras, "Formato del telefono es incorrecto", "Error", JOptionPane.ERROR_MESSAGE);
 								}
 								else JOptionPane.showMessageDialog(atras, "Formato de la edad es incorrecto", "Error", JOptionPane.ERROR_MESSAGE);
